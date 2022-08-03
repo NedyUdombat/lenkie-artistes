e90577c1deb69cb5058604a7d81a184d28364f2b
nedyudombat <nedyudombat@gmail.com>
2022-08-03 03:12:03 +0100

    Update UI for Not found page

--- src/pages/errors/NotFound.tsx
+++ src/pages/errors/NotFound.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
+import Topbar from "../../components/topbar";
 
 /** Util(s) */
 import { ROUTE_URLS } from "../../routes/RouteUrls";
 
 const NotFound = (): JSX.Element => (
-  <Container className="bg-warning d-flex flex-column" fluid>
-    <nav className="navbar">
-      <Link className="navlink" to={ROUTE_URLS.HOME_URL}>
-        Lenkie Music
-      </Link>
-    </nav>
-    <Container className="main-section flex-1">
+  <Container
+    className="d-flex flex-column px-0 vh-100"
+    fluid
+  >
+    <Topbar />
+    <Container className="flex-fill d-flex justify-content-center align-items-center">
       <div className="error-card">
-        <h1 className="title">404</h1>
-        <p className="info">
+        <h1 className="fs-1">404</h1>
+        <p>
           Something went wrong. We can’t find the page you are looking for
         </p>
-        <Link to={ROUTE_URLS.HOME_URL} className="btn btn-green">
+        <Link to={ROUTE_URLS.HOME_URL} className="btn btn-secondary mx-auto d-block">
           Go back
         </Link>
       </div>
